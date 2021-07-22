05968b5b0f04dddd082136c1ba71421c61cb209d
Kim Sung Hyun <soakdma37@gmail.com>
2021-07-22 17:54:53 +0900

    useReducer 사용법 20강

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const initialState={
     username:'',
     email:''
   },
+  //input 창에 들어있는 값들
   users:[
     {
       id:1,
@@ -29,18 +30,96 @@ const initialState={
   ]
 };
 
-function reducer(state, action){
-  return state;
+const reducer=(state, action)=>{
+  switch (action.type){
+    case 'CHANGE_INPUT':
+      return {
+        ...state,
+        inputs:{
+          ...state.inputs,
+          [action.name]:action.value
+        }
+      }
+    case 'CREATE_USER':
+      return {
+        //업데이트된 state 값이 들어감
+        inputs:initialState.inputs,
+        //input 창 초기화
+        users:state.users.concat(action.user)
+      }
+    case 'TOGGLE_USER':
+      return {
+        ...state,
+        users:state.users.map(user=>
+          user.id===action.id?{...user, active:!user.active}:user
+        )
+      }
+    case 'REMOVE_USER':
+      return {
+        ...state,
+        users:state.users.filter(user=>user.id!==action.id)
+      }
+    default:
+      return state;
+  }
 }
 
 function App() {
   const [state, dispatch]=useReducer(reducer, initialState);
+  const nextID=useRef(3);
+  const {users}=state;
+  const {username, email}=state.inputs;
+
+  const onChange=useCallback((e)=>{
+    const {name, value}=e.target;
+    dispatch({
+      type:'CHANGE_INPUT',
+      name,
+      value
+    });
+  }, []);
+
+  const onCreate=useCallback(()=>{
+    dispatch({
+      type:'CREATE_USER',
+      user:{
+        id:nextID.current,
+        username,
+        email
+      }
+    })
+    nextID.current+=1;
+  }, [username, email]);
+
+  const onToggle=useCallback((id)=>{
+    dispatch({
+      type:'TOGGLE_USER',
+      id
+    })
+  }, []);
+
+  const onRemove=useCallback((id)=>{
+    dispatch({
+      type:'REMOVE_USER',
+      id
+    })
+  },[])
+
+  const count=useMemo(()=>countActiveUsers(users), [users]);
   return (
     <div>
       <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
       />
-      <UserList users={[]} />
-      <div>활성 사용자 수는 0</div>
+      <UserList
+        users={users}
+        onToggle={onToggle}
+        onRemove={onRemove}
+      />
+      <div>활성 사용자 수는 {count}</div>
     </div>
   );
 }
