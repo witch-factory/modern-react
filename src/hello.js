118042eef6552fea319a15234d28c4984e5f9da6
Kim Sung Hyun <soakdma37@gmail.com>
2021-07-22 19:59:03 +0900

    hello 컴포넌트를 클래스 형태로 구현

--- src/hello.js
+++ src/hello.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, {Component} from "react";
 
-const Hello=({color, name, isSpecial})=>{
-  return <div style={{color}}>{isSpecial && <b>special </b>}name is {name}</div>
+class Hello extends Component{
+  static defaultProps={
+    name:"이름없음"
+  }
+  render(){
+    const {color, name, isSpecial}=this.props;
+    return (
+      <div style={{color}}>
+        {isSpecial && <b>*</b>}
+        Hello 페이지 {name}
+      </div>
+    )
+  }
 }
 
 export default Hello;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Hello from "./hello";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Hello isSpecial={true} color={'pink'}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
