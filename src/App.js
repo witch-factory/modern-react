ebcad4562baca584f4529554f51b5cc62b41eddf
Kim Sung Hyun <soakdma37@gmail.com>
2021-07-23 16:23:02 +0900

    새로운 할 일 추가 폼

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled, {createGlobalStyle} from 'styled-components';
 import TodoTemplate from "./components/TodoTemplate.js";
 import TodoListHeader from "./components/TodoListHeader";
 import TodoList from "./components/TodoList";
+import TodoListAdd from "./components/TodoListAdd";
 
 const GlobalStyle=createGlobalStyle`
   body{
@@ -17,6 +18,7 @@ function App(){
       <TodoTemplate>
         <TodoListHeader/>
         <TodoList/>
+        <TodoListAdd />
       </TodoTemplate>
     </>
   )
