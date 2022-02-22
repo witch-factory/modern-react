5caace50a7f7a0b7e182739241b91b303d03897a
witch-factory <soakdma37@gmail.com>
2022-02-23 00:03:43 +0900

    체크박스 useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ColorList from "./ColorList";
 import AddColorForm from "./AddColorForm";
 
-function App(){
+function Checkbox(){
+  const [checked, setChecked]=useState(false);
+
+  useEffect(()=>{
+    localStorage.setItem("checkbox-value", checked);
+  })
+  //렌더링이 끝나고 발생하는 함수
+
   return (
     <>
-      <AddColorForm/>
-      <ColorList/>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={()=>setChecked(checked=>!checked)}
+      />
+      {checked?"checked" : "not checked"}
     </>
+  )
+}
+
+function App(){
+  return (
+    <Checkbox />
   )
 }
 
