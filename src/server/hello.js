f03c1dcef2275889ee5bbe6100a25766ac3797f6
witch-factory <soakdma37@gmail.com>
2022-03-05 21:49:14 +0900

    Node 책 2장

--- src/server/hello.js
+++ src/server/hello.js
@@ -2,8 +2,24 @@ import http from 'http';
 
 const port=process.env.PORT || 3000;
 const server=http.createServer((req, res)=>{
-  res.writeHead(200, {'Content-Type':'text/plain'})
-  res.end('hello world!')
+  const path=req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase()
+  //쿼리스트링 모두 무시
+
+  switch(path){
+    case "":
+      res.writeHead(200, {'Content-Type':'text/plain'})
+      res.end('Home Page')
+      break
+    case '/about':
+      res.writeHead(200, {'Content-Type':'text/plain'})
+      res.end('About')
+      break
+    default:
+      res.writeHead(200, {'Content-Type':'text/plain'})
+      res.end('Not Found')
+      break
+  }
+
 })
 
 server.listen(port, ()=>console.log(`server started on port ${port}`))
